65a16299e1b7deb1efc3265a16804a7beaf1518f
Ondrej Martinek <martinekondrej@outlook.cz>
2023-04-09 10:37:55 +0200

    cleanup

--- ZSWV/convert.js
+++ ZSWV/convert.js
@@ -1,44 +1,44 @@
 function desitkovaNaTrojkovou(cislo) {
-    let vysledek = "";
+    let result = "";
     let iserror = false;
     
     if (cislo === "") { // ověřuje, jestli vstup není prázdná hodnota
       iserror = true;
-      return {vysledek, iserror};
+      return {result, iserror};
     }
     
-    if (isNaN(cislo)) { // ověří, zda je číslo neplatné (obsahuje nepovolené znaky)
+    if (isNaN(cislo)) { // ověří zda je vstup číslo
       iserror = true;
-      return {vysledek, iserror};
+      return {result, iserror};
     }
     
     cislo = Number(cislo); // převede řetězec na číslo
     
-    if (cislo < 0) { // ověření, jestli čístlo není záporné. Tento algoritmus zpracovává pouze kladná čísla na vstupu
+    if (cislo < 0) { // kontrola, jestli čístlo není záporné. Tento algoritmus zpracovává pouze kladná čísla na vstupu
       iserror = true;
-      return {vysledek, iserror};
+      return {result, iserror};
     }
     
     while (cislo > 0) { // převod čísla
       let zbytek = cislo % 3;
-      vysledek = zbytek + vysledek;
+      result = zbytek + result;
       cislo = Math.floor(cislo / 3);
     }
     
-    return {vysledek, iserror};
+    return {result, iserror};
   }
   
   // testovací hodnoty (přidal jsem i errorové)
-  let dtoIn = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "", "abc", "-123"];
+  let dtoIn = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "", "asdf", "-1234"];
   
   // test správnosti převodu pro každé číslo, které tam vstupuje
   for (let i = 0; i < dtoIn.length; i++) {
     let desitkoveCislo = dtoIn[i];
-    let {vysledek, iserror} = desitkovaNaTrojkovou(desitkoveCislo);
+    let {result, iserror} = desitkovaNaTrojkovou(desitkoveCislo);
     if (iserror === true) {
       console.log("Error: neplatné číslo");
     } else {
-      console.log("Desítkové číslo:", desitkoveCislo, "Trojkové číslo:", vysledek);
+      console.log("Desítkové číslo:", desitkoveCislo, "Trojkové číslo:", result);
     }
   }
   
