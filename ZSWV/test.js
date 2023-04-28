af5c33c6ed522d2e3afee93d338a008902fb5202
Drew-lgtm <martinek.ondrej.cz@gmail.com>
2023-04-28 19:59:57 +0200

    testing update

--- ZSWV/test.js
+++ ZSWV/test.js
@@ -11,3 +11,55 @@ let student = {
 }
 
 console.log(student.name)
+
+let nr = 42;
+let str = "";
+console.log(Boolean(nr));
+console.log(Boolean(str));
+
+console.log("42" !== 42);
+console.log("----------------------------")
+console.log(false == [[[]]]);
+console.log(true == [1]);
+console.log([4, 2] == "4,2");
+console.log([42] == 42);
+
+
+let name = "Arthur";
+let greeting = "Hello";
+console.log(`${greeting} ${name}!`);
+
+let nr1 = 30;
+let nr2 = 12;
+console.log(`Answer is ${nr1 + nr2}.`);
+console.log(42 % 9);
+let answer = 42;
+++answer;
+console.log(answer);
+console.log(true || false && !true);
+console.log((true || false) && !true);
+console.log(answer || name);
+
+let pocitadlo = 0;
+while(pocitadlo < 3) {
+  pocitadlo++;
+  if (pocitadlo === 1) {
+    continue;
+  }
+  console.log("ahoj");
+}
+
+let counter = 0;
+cyklus: while(counter < 3) {
+  let nextCounter = 0;
+  while(nextCounter < 3) {
+    if (nextCounter === 2) {
+      break cyklus;
+    }
+    console.log("hello");
+    nextCounter++;
+  }
+  counter++;
+}
+
+
--- ZSWV/test2.js
+++ ZSWV/test2.js
@@ -1,14 +1,40 @@
-function printAll(){
-  let x = 3;
-  console.log(x);
-  let y = 4;
-  console.log(y);
-  let x = 5;
-  console.log(x);
-  console.log(aaa);
+let answer = 42;
+answer %= 10;
+console.log(answer);
+
+let name = "Arthur";
+let age = 30;
+let planet = name === "Ford" ? "Betelgeuse VII"
+  : name === "Trillian" || name === "Arthur" ? "Earth"
+  : age < 0 ? "Magrathea"
+  : "Vogsphere";
+
+  for (let i = 0; i < 2; i++) {
+    console.log("ahoj");
+  }
+
+  const person = {fname:"John", lname:"Doe", age:25};
+
+let text = "";
+let x;
+for (x in person) {
+  text += person[x];
 }
 
-printAll();
+console.log(text);
 
-const answer = "waiting";
-answer = 42;
+let pocitadlo = 0;
+while (pocitadlo < 3) {
+  pocitadlo++;
+  console.log("ahoj");
+}
+console.log("-------------------------------")
+
+let pocitadlo2 = 0;
+while(pocitadlo2 < 10) {
+  console.log("ahoj");
+  if (pocitadlo2 === 2) {
+    break;
+  }
+  pocitadlo2++;
+}
