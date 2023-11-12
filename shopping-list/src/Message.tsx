85a0787cf12d657a60bcc76815bdd7e5dec9ba35
Drew-lgtm <martinek.ondrej.cz@gmail.com>
2023-11-12 16:26:29 +0100

    Creating project structure

---
+++ shopping-list/src/Message.tsx
@@ -0,0 +1,13 @@
+
+
+//PascalCasing
+function Message () {
+    // JSX> JavaScript XML
+    const name = 'Drew'
+    if (name)
+        return  <h1>Hello there {name}</h1>;
+    return <h1>Hello Word</h1>;
+
+}
+
+export default Message;
